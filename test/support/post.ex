18252ef4643bb5d0cec0bd88e3e5a4da6679c7f8
Jim Lemmers <jim.lemmers@gmail.com>
2025-08-04 17:13:05 +0200

    Maybe fix CI pipeline

--- test/support/post.ex
+++ test/support/post.ex
@@ -6,7 +6,7 @@ defmodule Post do
     field(:name, :string)
     field(:body, :string)
     belongs_to(:person, Person, on_replace: :update)
-    many_to_many(:tags, Tag, join_through: PostsTags)
-    many_to_many(:labels, Label, join_through: "posts_labels")
+    many_to_many(:tags, Tag, join_through: PostsTags, preload_order: [asc: :id])
+    many_to_many(:labels, Label, join_through: "posts_labels", preload_order: [asc: :id])
   end
 end
